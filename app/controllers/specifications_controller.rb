830b90094e161a042c8b9ed26003ae46f35181a3
Tyler Long <tyler4long@gmail.com>
2016-04-20 16:28:34 +0800

    Add view, edit for specification

--- app/controllers/specifications_controller.rb
+++ app/controllers/specifications_controller.rb
@@ -4,4 +4,25 @@ class SpecificationsController < ApplicationController
 
   def new
   end
+
+  def create
+    spec = Specification.new(spec_params)
+    spec.save!
+    redirect_to specifications_url
+  end
+
+  def show
+    @spec = Specification.find(params[:id])
+  end
+
+  def edit
+    @spec = Specification.find(params[:id])
+  end
+
+  private
+
+  def spec_params
+    params.permit(:version, :title, :description,
+      :termsOfService, :host, :basePath, :schemes, :consumes, :produces)
+  end
 end
