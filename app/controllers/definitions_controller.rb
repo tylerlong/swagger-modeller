e5f9de7d19a900c644630c434e9e67ae0eff88e6
Tyler Long <tyler4long@gmail.com>
2016-04-20 20:23:07 +0800

    Create definition

--- app/controllers/definitions_controller.rb
+++ app/controllers/definitions_controller.rb
@@ -2,4 +2,17 @@ class DefinitionsController < ApplicationController
   def new
     @spec = Specification.find(params[:specification_id])
   end
+
+  def create
+    defi = Definition.new(defi_params)
+    defi.save!
+    spec = Specification.find(params[:specification_id])
+    redirect_to spec
+  end
+
+  private
+
+  def defi_params
+    params.permit(:name, :description, :specification_id)
+  end
 end
