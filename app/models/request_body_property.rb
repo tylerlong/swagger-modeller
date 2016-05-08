f9a79fedc56b5b9b2e0d01a2eaf497d2200f3bec
Tyler Long <tyler4long@gmail.com>
2016-05-08 19:49:03 +0800

    Add lib/util.rb

--- app/models/request_body_property.rb
+++ app/models/request_body_property.rb
@@ -8,54 +8,9 @@ class RequestBodyProperty < ActiveRecord::Base
   validates :description, presence: true
   belongs_to :verb
 
-  # todo: remove duplicate code
+  require_dependency 'util'
   def self.parse(row)
-    name, type, description = row.split("\t").collect(&:strip)
-    if name == nil || type == nil || description == nil
-      return nil
-    end
-    rbp = RequestBodyProperty.new
-    rbp.name = name
-    rbp.description = description
-    if description.start_with?('Required.')
-      rbp.required = true
-    end
-    if type.start_with?('Collection of ') # array
-      rbp.type = 'array'
-      rbp.format = type[14..-1].gsub(/\s+/, '')
-      if rbp.format == 'URIs'
-        rbp.format = 'string'
-      end
-      return rbp
-    end
-
-    if type == 'True | False' || type == "'True' | 'False'" # boolean
-      rbp.type = 'boolean'
-      return rbp
-    end
-
-    if type.start_with?("'") and type.end_with?("'") # enum
-      rbp.type = 'string'
-      rbp.format = type
-      return rbp
-    end
-
-    if type == 'datetime' # datetime
-      rbp.type = 'string'
-      rbp.format = 'date-time'
-      return rbp
-    end
-
-    if ['string', 'integer', 'number', 'boolean'].include?(type) # primitive data types
-      rbp.type = type
-      if rbp.type == 'integer' && rbp.name == 'conversationId'
-        rbp.format = 'int64'
-      end
-      return rbp
-    end
-
-    rbp.type = type.gsub(/\s+/, '') # custom data types
-    return rbp
+    dict = ModelProperty.parse(row)
+    dict.nil? ? nil : RequestBodyProperty.new(dict)
   end
-
 end
---
+++ lib/util.rb
@@ -0,0 +1,51 @@
+module ModelProperty
+
+  def parse(row)
+    name, type, description = row.split("\t").collect(&:strip)
+    if name == nil || type == nil || description == nil
+      return nil
+    end
+    dict = { name: name, description: description }
+    if description.start_with?('Required.')
+      dict[:required] = true
+    end
+    if type.start_with?('Collection of ') # array
+      dict[:type] = 'array'
+      dict[:format] = type[14..-1].gsub(/\s+/, '')
+      if dict[:foramt] == 'URIs'
+        dict[:foramt] = 'string'
+      end
+      return dict
+    end
+
+    if type == 'True | False' || type == "'True' | 'False'" # boolean
+      dict[:type] = 'boolean'
+      return dict
+    end
+
+    if type.start_with?("'") and type.end_with?("'") # enum
+      dict[:type] = 'string'
+      dict[:format] = type
+      return dict
+    end
+
+    if type == 'datetime' # datetime
+      dict[:type] = 'string'
+      dict[:format] = 'date-time'
+      return dict
+    end
+
+    if ['string', 'integer', 'number', 'boolean'].include?(type) # primitive data types
+      dict[:type] = type
+      if dict[:type] == 'integer' && dict[:name] == 'conversationId'
+        dict[:format] = 'int64'
+      end
+      return dict
+    end
+
+    dict[:type] = type.gsub(/\s+/, '') # custom data types
+    return dict
+  end
+  module_function :parse
+
+end
