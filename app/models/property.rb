68703689b1958d277f949415af6cf76a624dc2a3
Tyler Long <tyler4long@gmail.com>
2016-05-08 19:57:07 +0800

    Refactor code

--- app/models/property.rb
+++ app/models/property.rb
@@ -7,54 +7,9 @@ class Property < ActiveRecord::Base
   validates :type, presence: true
   belongs_to :definition
 
-  # todo: remove duplicate code
+  require_dependency 'util'
   def self.parse(row)
-    name, type, description = row.split("\t").collect(&:strip)
-    if name == nil || type == nil || description == nil
-      return nil
-    end
-    prop = Property.new
-    prop.name = name
-    prop.description = description
-    if description.start_with?('Optional.')
-      prop.required = false
-    end
-    if type.start_with?('Collection of ') # array
-      prop.type = 'array'
-      prop.format = type[14..-1].gsub(/\s+/, '')
-      if prop.format == 'URIs'
-        prop.format = 'string'
-      end
-      return prop
-    end
-
-    if type == 'True | False' || type == "'True' | 'False'" # boolean
-      prop.type = 'boolean'
-      return prop
-    end
-
-    if type.start_with?("'") and type.end_with?("'") # enum
-      prop.type = 'string'
-      prop.format = type
-      return prop
-    end
-
-    if type == 'datetime' # datetime
-      prop.type = 'string'
-      prop.format = 'date-time'
-      return prop
-    end
-
-    if ['string', 'integer', 'number', 'boolean'].include?(type) # primitive data types
-      prop.type = type
-      if prop.type == 'integer' && prop.name == 'conversationId'
-        prop.format = 'int64'
-      end
-      return prop
-    end
-
-    prop.type = type.gsub(/\s+/, '') # custom data types
-    return prop
+    dict = ModelProperty.parse(row)
+    dict.nil? ? nil : Property.new(dict)
   end
-
 end
--- app/models/query_parameter.rb
+++ app/models/query_parameter.rb
@@ -8,40 +8,9 @@ class QueryParameter < ActiveRecord::Base
   validates :description, presence: true
   belongs_to :verb
 
-  # todo: remove duplicate code
+  require_dependency 'util'
   def self.parse(row)
-    name, type, description = row.split("\t").collect(&:strip)
-    if name == nil || type == nil || description == nil
-      return nil
-    end
-    qp = QueryParameter.new
-    qp.name = name
-    qp.description = description
-    if description.start_with?('Required.')
-      qp.required = true
-    end
-
-    if type.start_with?("'") and type.end_with?("'") # enum
-      qp.type = 'string'
-      qp.format = type
-      return qp
-    end
-
-    if type == 'datetime' # datetime
-      qp.type = 'string'
-      qp.format = 'date-time'
-      return qp
-    end
-
-    if ['string', 'integer', 'boolean'].include?(type) # primitive data types
-      qp.type = type
-      if qp.type == 'integer' && qp.name == 'conversationId'
-        qp.format = 'int64'
-      end
-      return qp
-    end
-
-    return nil
+    dict = ModelProperty.parse(row)
+    dict.nil? ? nil : QueryParameter.new(dict)
   end
-
 end
