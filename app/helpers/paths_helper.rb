1cf210d72424efff75e8341b848962d096d0e817
Tyler Long <tyler4long@gmail.com>
2016-05-10 23:15:39 +0800

    Show path status

--- app/helpers/paths_helper.rb
+++ app/helpers/paths_helper.rb
@@ -1,2 +1,12 @@
 module PathsHelper
+  def path_status(path)
+    status = 'warning'
+    green = path.verbs.detect do |verb|
+      verb.visibility.include?('Basic') && (verb.status == '' || verb.status == 'Normal')
+    end
+    if green.present?
+      status = 'success'
+    end
+    status
+  end
 end
