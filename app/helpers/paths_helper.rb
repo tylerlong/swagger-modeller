8cb667189fc4459a06148b8aa1472790f78d4846
Tyler Long <tyler4long@gmail.com>
2016-05-12 14:28:22 +0800

    Update path status

--- app/helpers/paths_helper.rb
+++ app/helpers/paths_helper.rb
@@ -1,12 +1,17 @@
 module PathsHelper
   def path_status(path)
-    status = 'warning'
+    status = 'danger'
+
+    yellow = path.verbs.detect do |verb|
+      verb.visibility.include?('Advanced') && (verb.status == '' || verb.status == 'Normal')
+    end
+    status = 'warning' if yellow.present?
+
     green = path.verbs.detect do |verb|
       verb.visibility.include?('Basic') && (verb.status == '' || verb.status == 'Normal')
     end
-    if green.present?
-      status = 'success'
-    end
-    status
+    status = 'success' if green.present?
+
+    return status
   end
 end
