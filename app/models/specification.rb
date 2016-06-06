fcad54f102092901b23cda6acd12be7bddfcec50
Tyler Long <tyler4long@gmail.com>
2016-06-06 15:31:51 +0800

    Add security to swagger

--- app/models/specification.rb
+++ app/models/specification.rb
@@ -77,6 +77,9 @@ class Specification < ActiveRecord::Base
           },
         },
       },
+      security: [{
+        oauth: ['default'],
+      },],
       parameters: PathParameter.swagger,
       definitions: CommonModel.swagger,
       paths: {},
