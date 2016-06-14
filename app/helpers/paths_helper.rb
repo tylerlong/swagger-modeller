6d0abcc0cc893824d17adae040e78be17a120da1
Tyler Long <tyler4long@gmail.com>
2016-06-14 17:39:16 +0800

    Improve path labels

--- app/helpers/paths_helper.rb
+++ app/helpers/paths_helper.rb
@@ -1,23 +1,26 @@
 module PathsHelper
   def path_status(path)
-    status = 'danger'
+    green = path.verbs.detect do |verb|
+      verb.visibility.include?('Basic') && (verb.status == '' || verb.status == 'Normal')
+    end
+    if green.present?
+      return raw "<span class=\"label label-success\">Basic</span>"
+    end
 
     yellow = path.verbs.detect do |verb|
       verb.visibility.include?('Advanced') && (verb.status == '' || verb.status == 'Normal')
     end
-    status = 'warning' if yellow.present?
-
-    green = path.verbs.detect do |verb|
-      verb.visibility.include?('Basic') && (verb.status == '' || verb.status == 'Normal')
-    end
-    status = 'success' if green.present?
-
-    if status == 'success'
-      return raw "<span class=\"label label-success\">Basic</span>"
-    elsif status == 'warning'
+    if yellow.present?
       return raw "<span class=\"label label-warning\">Advanced</span>"
-    else # danger
-      return raw "<span class=\"label label-danger\">Internal / Deprecated / Disabed / NoDoc</span>"
     end
+
+    internal = path.verbs.detect do |verb|
+      verb.visibility.include?('Internal')
+    end
+    if internal.present?
+      return raw "<span class=\"label label-danger\">Internal</span>"
+    end
+
+    return raw "<span class=\"label label-danger\">#{path.verbs.first.status}</span>"
   end
 end
