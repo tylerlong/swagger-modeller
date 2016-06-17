88b6d88d77eb4b7970059cbf1558d4316559bce7
Tyler Long <tyler4long@gmail.com>
2016-06-17 17:40:03 +0800

    Refactor code

--- app/models/model.rb
+++ app/models/model.rb
@@ -28,23 +28,35 @@ class Model < ActiveRecord::Base
   end
 
   def referenced_by_model
-    ModelProperty.where('data_type = ? or format = ?', name, name).collect(&:model).uniq
+    ModelProperty.joins(%{join models on model_properties.model_id = models.id})
+      .where('models.specification_id = ? and (data_type = ? or format = ?)', specification_id, name, name)
+      .collect(&:model).uniq
   end
 
   def referenced_by_request_body
-    verbs = Verb.joins('left join request_body_properties as rbp on rbp.verb_id = verbs.id').where('rbp.id is null and verbs.request_body_text <> ?', '')
+    verbs = Verb.joins('left join request_body_properties as rbp on rbp.verb_id = verbs.id')
+        .where('rbp.id is null and verbs.request_body_text <> ?', '')
     verbs = verbs.select do |verb|
       verb.request_body_text.split("\n").collect(&:strip).include? name
     end
-    verbs.concat(RequestBodyProperty.where('data_type = ? or format = ?', name, name).collect(&:verb)).uniq
+    verbs2 = RequestBodyProperty.joins(%{join verbs on request_body_properties.verb_id = verbs.id
+                                         join paths on verbs.path_id = paths.id})
+        .where('paths.specification_id = ? and (data_type = ? or format = ?)', specification_id, name, name)
+        .collect(&:verb)
+    verbs.concat(verbs2).uniq
   end
 
   def referenced_by_response_body
-    verbs = Verb.joins('left join response_body_properties as rbp on rbp.verb_id = verbs.id').where('rbp.id is null and verbs.response_body_text <> ?', '')
+    verbs = Verb.joins('left join response_body_properties as rbp on rbp.verb_id = verbs.id')
+        .where('rbp.id is null and verbs.response_body_text <> ?', '')
     verbs = verbs.select do |verb|
       verb.response_body_text.split("\n").collect(&:strip).include? name
     end
-    verbs.concat(ResponseBodyProperty.where('data_type = ? or format = ?', name, name).collect(&:verb)).uniq
+    verbs2 = ResponseBodyProperty.joins(%{join verbs on response_body_properties.verb_id = verbs.id
+                                          join paths on verbs.path_id = paths.id})
+        .where('paths.specification_id = ? and (data_type = ? or format = ?)', specification_id, name, name)
+        .collect(&:verb)
+    verbs.concat(verbs2).uniq
   end
 
 end
